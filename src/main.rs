36a23a96e04e5f98d244543571986e753b55f35b
FRANCISCO CARLOS PIERNA <pierna_f@hotmail.com>
2024-06-16 09:06:07 -0300

    exec 16/06/2024

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
-}
+    let nome = "Fabio";
+    println!("nome {}", nome);
+   // nome="Fabio"; variável em rust imutavel isso dá erro e nem somar 1 em inteiro
+   // para mudar para mutavel inseri mut exemplo
+   let mut nome1  = "Fabio";
+   nome1=" Henrique";
+   println!("nome1 {}", nome1); 
+
+
+    }
