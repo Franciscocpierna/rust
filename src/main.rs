d888c51efd7ecc9ff689ba9401525b187feb6278
FRANCISCO CARLOS PIERNA <pierna_f@hotmail.com>
2024-06-18 19:52:21 -0300

    exec 12/06/2024

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
+use std::io;
+fn convert_to_int(data_input: & String)->i32{
+    let x=data_input.trim().parse::<i32>().unwrap(); 
+    x 
+}
+
 fn main() {
-    let nome = "Fabio";
-    println!("nome {}", nome);
-   // nome="Fabio"; variável em rust imutavel isso dá erro e nem somar 1 em inteiro
-   // para mudar para mutavel inseri mut exemplo
-   let mut nome1  = "Fabio";
-   nome1=" Henrique";
-   println!("nome1 {}", nome1); 
-
-
-    }
+   let mut number1 = String::new();
+   io::stdin().read_line(&mut number1).expect("Erro ao ler number1");
+   let mut number2 = String::new();
+   io::stdin().read_line(&mut number2).expect("Erro ao ler number2");
+   if convert_to_int(30&number1) > convert_to_int(&number2){
+     println!("o numero {} é maior que {}",number1,number2);
+   }
+     println!("o numero {} é menor ou igual que {}",number1,number2);
+ 
+   }
