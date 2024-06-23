be2131c7efebb216944a9b11c34c661b4b630f0b
FRANCISCO CARLOS PIERNA <pierna_f@hotmail.com>
2024-06-23 16:49:34 -0300

    exercicio função 23/06/2024

--- exercicio3/src/main.rs
+++ exercicio3/src/main.rs
@@ -1,5 +1,16 @@
+fn dobro(num: i32) -> i32{
+    return num*2;
+}
+
+fn maior(a: i32, b: i32) -> i32{
+    if a >= b {
+        return a;
+    }else{
+        return  b;
+    }
+}
 fn main() {
-    let mut a=15;
+   /* let mut a=15;
     let mut b=40;
     while b != 0 {
         let temp = b;
@@ -7,5 +18,7 @@ fn main() {
         println!("b = {} ",b);
         a=temp;
     }
-    println!("Maior divisor comum : {} ",a);
+    println!("Maior divisor comum : {} ",a); */
+    println!("o dobro de 5 é {}", dobro(5)); 
+    println!("o dobro de 4 e 5 é {}", maior(4,5)); 
 }
