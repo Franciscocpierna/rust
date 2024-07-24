e24bdb04898c86b20c4da0c412c38ceb1a66228a
FRANCISCO CARLOS PIERNA <pierna_f@hotmail.com>
2024-07-24 09:41:17 -0300

    24/07/2024

--- compress/src/main.rs
+++ compress/src/main.rs
@@ -69,7 +69,7 @@ fn compress_string(original_str: &str)->String{
 
 
 fn main() {
-    let original_str = "aabccccccaaa";
+    let original_str = "aaabccccaaaa";
     let compressed_str = compress_string(original_str);
     println!("Original: {}", original_str);
     println!("Compressed: {}", compressed_str);
@@ -81,3 +81,36 @@ fn main() {
 }
 
 
+/*
+fn compress_string(s: &str)->String{
+  let mut compressed = String::new();
+  let mut count=0; 
+ // percorre a string para realizar a compressão
+  for (i,c) in s.chars().enumerate(){
+   count +=1;
+   //verificar se o pçroximo caracter é diferente ou chegamos ao final da string
+   if i +1 >= s.len()||c!=s.chars().nth(i+1).unwrap(){
+        compressed.push(c);
+        compressed.push_str(&count.to_string());
+        count=0;// reinicia a contagem para o proximo cartacter
+   }
+  }
+    if compressed.len()>=s.len(){
+        s.to_string();//ou usa return
+    }else{
+      compressed // ou usa return
+    }
+
+}
+fn main() {
+    let original_str = "aaabccccaaaa";
+    let compressed_str = compress_string(original_str);
+    println!("Original: {}", original_str);
+    println!("Compressed: {}", compressed_str);
+
+    let other_str = "abcdefgh";
+    let compressed_other = compress_string(other_str);
+    println!("Original: {}", other_str);
+    println!("Compressed: {}", compressed_other);
+}
+*/
